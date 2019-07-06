a830221bf116a3a19f15db135da1a4dddbbcabec
jatin chopra <jatinchopra3@gmail.com>
2019-07-06 14:53:13 +0530

    First Commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCB-Zsz1Axz1ZwPlinmMS7fMJGiU3PyjfY",
+    authDomain: "angular-image-gallery-ce929.firebaseapp.com",
+    databaseURL: "https://angular-image-gallery-ce929.firebaseio.com",
+    projectId: "angular-image-gallery-ce929",
+    storageBucket: "angular-image-gallery-ce929.appspot.com",
+    messagingSenderId: "387518876873",
+    appId: "1:387518876873:web:f05807ac0f8eb456"
+  } 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyCB-Zsz1Axz1ZwPlinmMS7fMJGiU3PyjfY",
+    authDomain: "angular-image-gallery-ce929.firebaseapp.com",
+    databaseURL: "https://angular-image-gallery-ce929.firebaseio.com",
+    projectId: "angular-image-gallery-ce929",
+    storageBucket: "angular-image-gallery-ce929.appspot.com",
+    messagingSenderId: "387518876873",
+    appId: "1:387518876873:web:f05807ac0f8eb456"
+  } 
 };
 
 /*
